a30c8a67fbe98c17c363b0fbe0d1c1822412cd76
Yashwith kumar k v <yashwithfist@gmail.com>
2026-02-13 15:11:52 +0530

    electron update changes

--- Project-Dashboard-and-Tracker-main/frontend/main.js
+++ Project-Dashboard-and-Tracker-main/frontend/main.js
@@ -1,8 +1,9 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import { app, BrowserWindow, powerMonitor } from "electron";
-import { autoUpdater } from "electron-updater";
+import updater from "electron-updater";
 
+const { autoUpdater } = updater;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
